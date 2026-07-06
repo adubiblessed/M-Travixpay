95a6f73c150d61dd9fd28568f356bbcb1e174e4a
adubi <adubiblessed@gmail.com>
2026-07-06 19:17:28 +0100

    url route modification

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,10 @@ from accounts import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('auth/login/', views.login_view, name='login'),
-    path('auth/register/', views.register_view, name='register'),
-    path('auth/forgot-password/', views.forgot_password_view, name='forgot_password'),
-    path('auth/logout/', views.logout_view, name='logout'),
+    path('accounts/login/', views.login_view, name='login'),
+    path('accounts/register/', views.register_view, name='register'),
+    path('accounts/forgot-password/', views.forgot_password_view, name='forgot_password'),
+    path('accounts/logout/', views.logout_view, name='logout'),
     path('dashboard/', views.passenger_dashboard_view, name='passenger_dashboard'),
+    path('', views.passenger_dashboard_view, name='passenger_dashboard'),
 ]
