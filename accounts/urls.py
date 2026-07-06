7ea193e0b762d32fca7a8ac8808d04212e7e9d1f
adubi <adubiblessed@gmail.com>
2026-07-06 20:08:43 +0100

    auth path changed

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('accounts/login/', views.login_view, name='login'),
     path('accounts/register/', views.register_view, name='register'),
     path('accounts/forgot-password/', views.forgot_password_view, name='forgot_password'),
-    path('accounts/logout/', views.logout_view, name='logout'),
+    path('auth/logout/', views.logout_view, name='logout'),
     path('dashboard/', views.passenger_dashboard_view, name='passenger_dashboard'),
     path('', views.passenger_dashboard_view, name='passenger_dashboard'),
 ]
